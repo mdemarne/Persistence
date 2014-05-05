1b3a23645edd87c6e9e0c7bc6bc7c5aabe24ad8d
Mathieu Demarne <mathieu.demarne@gmail.com>
2014-05-05 14:35:41 +0200

    ToolBox and Recomposer takes a universe in params.
    All in the title.

--- library/src/main/scala/reflect/persistence/ToolBox.scala
+++ library/src/main/scala/reflect/persistence/ToolBox.scala
@@ -1,4 +1,9 @@
 package scala.reflect.persistence
 
 /* Fetch trees and subtrees from the decompressed AST. */
-class ToolBox { /*TODO */ }
+/* For now : return the entire AST, take a universe as argument */
+class ToolBox(val u: scala.reflect.api.Universe) {
+  import u._
+  import Enrichments._
+  /*TODO */
+}
