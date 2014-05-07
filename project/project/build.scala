5415a710de36daf1c637844e32a2d4fd9305386c
Mathieu Demarne <mathieu.demarne@gmail.com>
2014-05-07 16:52:26 +0200

    Little modification in the sbt plugin settings.
    Doest work for the aggregated root project, but not for subproject... it seems.

--- project/project/build.scala
+++ project/project/build.scala
@@ -1,5 +1,5 @@
 import sbt._
 object PluginDef extends Build {
-    lazy val root = project.in( file(".") ).dependsOn( sbtPPlugin )
+    lazy val root = project.in(file(".")).dependsOn( sbtPPlugin )
     lazy val sbtPPlugin = file("../sbt")
 }
--- sbt/src/main/scala/reflect/persistence/sbt/Plugin.scala
+++ sbt/src/main/scala/reflect/persistence/sbt/Plugin.scala
@@ -3,9 +3,9 @@ package scala.reflect.persistence.sbt /* TODO: check for proper package */
 import sbt._
 import Keys._
 
-class PersistencePlugin extends Plugin {
-  println("hello")
- override lazy val settings = Seq(commands += myCommand)
+object PersistencePlugin extends Plugin
+{
+  override lazy val settings = Seq(commands += myCommand)
 
   lazy val myCommand =
     Command.command("hello") { (state: State) =>
