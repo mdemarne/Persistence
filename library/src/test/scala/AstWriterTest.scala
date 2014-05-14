0a8a3f9a37bb61be77a986aaafc4bd49a36bc748
Adrien Ghosn <ghosn.adrien@gmail.com>
2014-05-14 13:14:02 +0200

    Changed the test ASTWriter
    That become obsolete with the new way we do the compression

--- library/src/test/scala/AstWriterTest.scala
+++ library/src/test/scala/AstWriterTest.scala
@@ -13,30 +13,15 @@ import java.io.FileInputStream
 class AstWriterTest extends FunSuite {
 
   test("WritingTest") {
-    val treeStr = "c (m (v v (c (m v v) c (m v v))) m(v v (c c)) c (m (v v (c (m v v) c (m v v))) m(v v (c c)) ))"
+  val treeStr = "c (m (v v (c (m v v) c (m v v))) m(v v (c c)) c (m (v v (c (m v v) c (m v v))) m(v v (c c)) ))"
     val tree = ParseTestTree.parse(treeStr).get
     
     val file = new File("WritingTest.ast")
     val compressor = new AstCompressor(new DataOutputStream(new FileOutputStream(file)))
     val decompressor = new AstDecompressor(new DataInputStream(new FileInputStream(file)))
-    
-    val splitTree = compressor.splitTree(tree)
-    val hufCodes = compressor.genHuffman(splitTree._1)
-    val encOccs = compressor.encodeOccs(splitTree._2, hufCodes)
-    
-    compressor.outputOccs(encOccs)
-    compressor.outputEdges(splitTree._3)
-    compressor.outputDict(hufCodes)
-    
-    val dOccs = decompressor.inputOccs
-    val dEdges = decompressor.inputEdges
-    val dDict = decompressor.inputDict
-    val transCodes = hufCodes.map{e => 
-      (e._1.map(n => NodeBFS(Node(n.node.tpe, Nil), n.bfsIdx, n.parentBfsIdx)).toList, e._2)}.toMap
-    assert(encOccs == dOccs, "Occurences do not match")
-    assert(splitTree._3.tail == dEdges, "Edges do not match")
-    assert(transCodes.map(_.swap) == dDict, "Dict do not match")
-    
-    file.delete()    
+    compressor(tree)
+    val recupTree = decompressor()
+    assert(tree == recupTree, s"Error: Not matching:\n${tree}\nAnd\n${recupTree}")    
+    file.delete()
   }
 }
