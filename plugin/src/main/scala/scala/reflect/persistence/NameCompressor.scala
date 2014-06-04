0eddd731f4f9954fe04f25d2cffbb4c85cd7e2ab
Mathieu Demarne <mathieu.demarne@gmail.com>
2014-06-04 17:41:21 +0200

    Fix bug in NameCompressor
    All in the title.

--- plugin/src/main/scala/scala/reflect/persistence/NameCompressor.scala
+++ plugin/src/main/scala/scala/reflect/persistence/NameCompressor.scala
@@ -5,12 +5,13 @@ class NameCompressor {
   
   def apply(names: Map[String, List[Int]]): List[Byte] = {
     val namesOnly = names.keys.toList.sortBy(e => e)
-    val namesIds = namesOnly.zipWithIndex.map(x => x.swap).toMap
+    val namesOccs = names.flatMap(v => v._2 .map(x => (x, v._1)))
+    val namesIds = namesOnly.zipWithIndex.toMap
     val maxId = names.values.flatten.max
     val occs = (0 to maxId).map { i =>
-    	namesIds.get(i) match {
+    	namesOccs.get(i) match {
         case None => -1 
-        case Some(id) => i
+        case Some(name) => namesIds(name)
       }}.toList
     val v1 = namesOnly.mkString("\n").getBytes().toList 
     val v2 = occs.flatMap(o => ShortToBytes(o.toShort))
