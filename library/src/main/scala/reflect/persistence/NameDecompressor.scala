2a4483b413190591b3919a61316fbc1e5485dbf9
Adrien Ghosn <ghosn.adrien@gmail.com>
2014-06-04 17:40:35 +0200

    Did the decompression
    For the names

--- library/src/main/scala/reflect/persistence/NameDecompressor.scala
+++ library/src/main/scala/reflect/persistence/NameDecompressor.scala
@@ -5,7 +5,7 @@ class NameDecompressor {
   
   def apply(toRead: List[Byte]): (Map[String, List[Int]], List[Byte]) = {
     val (size, xs) = readInt(toRead)
-    var rest: List[Byte] = xs
+   /* var rest: List[Byte] = xs
     val res: Map[String, List[Int]] = (0 until size).map{ y => 
       val name: String = rest.takeWhile(_ != '\n'.toByte).map(_.toChar).mkString
       rest = rest.dropWhile(_ != '\n').tail
@@ -18,6 +18,21 @@ class NameDecompressor {
       }.toList
       (name -> entries)
     }.toMap
-    (res, rest)
+    (res, rest)*/
+    val namesByte: List[Byte] = xs.take(size)
+    var rest: List[Byte] = xs.drop(size)
+    println(s"The rest ${rest}")
+    val namesList: List[String] = namesByte.map(_.toChar).mkString.split("\n").toList.sorted
+    val (occSize, r) = readInt(rest)
+    rest = r
+    val occs: List[(Int, Int)] = (0 until occSize).map{ i =>
+      println(s"The rest ${rest}")
+      val (entry, rs) = readShort(rest)
+      println(s"The rest ${rs}")
+      rest = rs
+      (i, entry.toInt)
+    }.toList.filter(_._2 != -1)
+    val toZip: List[List[Int]] = occs.groupBy(_._2).toList.map(_._2.map(_._1)).toList
+    (namesList.zip(toZip).toMap, rest)
   }
 }
