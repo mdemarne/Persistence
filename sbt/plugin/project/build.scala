74119fc52ba80663c6f05d6f4178ea8e596ae000
Mathieu Demarne <mathieu.demarne@gmail.com>
2014-06-13 16:05:27 +0200

    Cleanup of the sbt plugin

--- sbt/plugin/project/build.scala
+++ sbt/plugin/project/build.scala
@@ -8,32 +8,30 @@ import complete.DefaultParsers._
 object build extends Build {
   lazy val sbtPPlugin = Project(
     id = "persistence-sbt",
-    base = file(".")
-  ) settings (
-  	scalaVersion := "2.10.3", // Forced for now due to SBT macro system compilation.
-    crossVersion := CrossVersion.full,
-    version := "0.1.0-SNAPSHOT",
-    organization := "org.scalareflect",
-    description := "AST persistence for Project Palladium",
-    resolvers += Resolver.sonatypeRepo("snapshots"),
-    resolvers += Resolver.sonatypeRepo("releases"),
-    scalacOptions ++= Seq("-deprecation", "-feature", "-optimise"),
-    // Thanks to https://github.com/gkossakowski/scala-sbt-cross-compile
-    // add scala-xml dependency when needed (for Scala 2.11 and newer) in a robust way
-	// this mechanism supports cross-version publishing
-	libraryDependencies := {
-	  CrossVersion.partialVersion(scalaVersion.value) match {
-	    // if scala 2.11+ is used, add dependency on scala-xml module
-	    case Some((2, scalaMajor)) if scalaMajor >= 11 =>
-	      libraryDependencies.value ++ Seq(
-	        "org.scala-lang.modules" %% "scala-xml" % "1.0.1",
-	        "org.scala-lang.modules" %% "scala-parser-combinators" % "1.0.1",
-	        "org.scala-lang.modules" %% "scala-swing" % "1.0.1")
-	    case _ =>
-	      // or just libraryDependencies.value if you don't depend on scala-swing
-	      libraryDependencies.value :+ "org.scala-lang" % "scala-swing" % scalaVersion.value
-	  }
-	},
-  	sbtPlugin := true
-  )
+    base = file(".")) settings (
+      scalaVersion := "2.10.3", // Forced for now due to SBT macro system compilation.
+      crossVersion := CrossVersion.full,
+      version := "0.1.0-SNAPSHOT",
+      organization := "org.scalareflect",
+      description := "AST persistence for Project Palladium",
+      resolvers += Resolver.sonatypeRepo("snapshots"),
+      resolvers += Resolver.sonatypeRepo("releases"),
+      scalacOptions ++= Seq("-deprecation", "-feature", "-optimise"),
+      // Thanks to https://github.com/gkossakowski/scala-sbt-cross-compile
+      // add scala-xml dependency when needed (for Scala 2.11 and newer) in a robust way
+      // this mechanism supports cross-version publishing
+      libraryDependencies := {
+        CrossVersion.partialVersion(scalaVersion.value) match {
+          // if scala 2.11+ is used, add dependency on scala-xml module
+          case Some((2, scalaMajor)) if scalaMajor >= 11 =>
+            libraryDependencies.value ++ Seq(
+              "org.scala-lang.modules" %% "scala-xml" % "1.0.1",
+              "org.scala-lang.modules" %% "scala-parser-combinators" % "1.0.1",
+              "org.scala-lang.modules" %% "scala-swing" % "1.0.1")
+          case _ =>
+            // or just libraryDependencies.value if you don't depend on scala-swing
+            libraryDependencies.value :+ "org.scala-lang" % "scala-swing" % scalaVersion.value
+        }
+      },
+      sbtPlugin := true)
 }
--- sbt/plugin/src/main/scala/reflect/persistence/sbt/AstcPlugin.scala
+++ sbt/plugin/src/main/scala/reflect/persistence/sbt/AstcPlugin.scala
@@ -1,4 +1,4 @@
-package scala.reflect.persistence.sbt /* TODO: check for proper package */
+package scala.reflect.persistence.sbt
 
 import sbt._
 import Keys._
@@ -7,29 +7,28 @@ import scala.language.existentials
 
 object AstcPlugin extends Plugin {
   override lazy val projectSettings = Seq(packageAstTask, beforeCompileTask) ++ usePluginSettings ++ newCompile ++ publishSettings ++ packageSettings
-
-  lazy val usePluginSettings = Seq(
-      addCompilerPlugin("org.scalareflect" % "persistence-plugin_2.11.0" % "0.1.0-SNAPSHOT")
-  )
-
-  /* TODO: check what we need as a Manifest(if relevant) */
+  lazy val usePluginSettings = Seq(addCompilerPlugin("org.scalareflect" % "persistence-plugin_2.11.0" % "0.1.0-SNAPSHOT"))
   val packageAst = TaskKey[File]("package-ast", "Produce an artifact containing compressed Scala ASTs.")
   val packageAstTask = packageAst := {
-    (compile in Compile).value     /* First let's compile everything */
-    val generalPath = new File((classDirectory in Compile).value.getParent).getAbsolutePath
-    val astsPath = generalPath + "/asts/"
-    val outputJar = new File(generalPath + "/" + name.value + "_" + version.value + "-asts.jar")
-    val astsSources = findFiles(new File(astsPath))
-    val log = streams.value.log
-    val manifest = new java.util.jar.Manifest()
-    Package.makeJar(astsSources.map(f => (f, f.getAbsolutePath.replace(astsPath, ""))), outputJar, manifest, log)
-    outputJar
-  }
-  def findFiles(root: File): List[File] = root match {
+    def findFiles(root: File): List[File] = root match {
       case _ if root.isDirectory => root.listFiles.toList.flatMap(f => findFiles(f))
       case _ => root :: Nil
+    }
+    val res = (compile in Compile).result.value match { /* First let's compile everything */
+      case Value(analysis) =>
+        val generalPath = new File((classDirectory in Compile).value.getParent).getAbsolutePath
+        val astsPath = generalPath + "/asts/"
+        val outputJar = new File(generalPath + "/" + name.value + "_" + version.value + "-asts.jar")
+        val astsSources = findFiles(new File(astsPath))
+        val log = streams.value.log
+        val manifest = new java.util.jar.Manifest()
+        Package.makeJar(astsSources.map(f => (f, f.getAbsolutePath.replace(astsPath, ""))), outputJar, manifest, log)
+        outputJar
+      case Inc(inc: Incomplete) => /* Cannot package, since we get compilation error */
+        throw inc /* Just re-throw exception */
+    }
+    res
   }
-  
   /* Save the previously generated .ast files */
   val beforeCompile = TaskKey[Unit]("before-compile")
   val beforeCompileTask = beforeCompile := {
@@ -42,26 +41,24 @@ object AstcPlugin extends Plugin {
       if (f.isDirectory) f.listFiles.foreach(deleteAll(_))
       f.delete
     }
-    beforeCompile.value /* Save the .asts */
+    beforeCompile.value /* Save the previous .asts (if any) */
     val generalPath = new File((classDirectory in Compile).value.getParent).getAbsolutePath
     var res = (compile in Compile).result.value match {
-    case Inc(inc: Incomplete) =>
-    (new File(generalPath + "/asts.bak")).renameTo(new File(generalPath + "/asts"))
-      throw inc
-    case Value(analysis) =>
-    if (new File(generalPath + "/asts").exists) deleteAll(new File(generalPath + "/asts.bak"))
-    else (new File(generalPath + "/asts.bak")).renameTo(new File(generalPath + "/asts"))
-      analysis
-  }
-  res
+      case Inc(inc: Incomplete) => /* Restore the old .asts */
+        (new File(generalPath + "/asts.bak")).renameTo(new File(generalPath + "/asts"))
+        throw inc
+      case Value(analysis) => /* If Compile was called without compiling anything, no new /asts folder will be generated */
+        if (new File(generalPath + "/asts").exists) deleteAll(new File(generalPath + "/asts.bak"))
+        else (new File(generalPath + "/asts.bak")).renameTo(new File(generalPath + "/asts"))
+        analysis
+    }
+    res
   })
-
   /* Force publishing the ast artifact */
   lazy val publishSettings = (artifact in (Compile, packageAst) ~= { art =>
-      art.copy(`classifier` = Some("asts"))
-    }) ++ addArtifact(artifact in (Compile, packageAst), packageAst).settings
-
-  /* In packageBin, force also packaging the Asts */
+    art.copy(`classifier` = Some("asts"))
+  }) ++ addArtifact(artifact in (Compile, packageAst), packageAst).settings
+  /* In package, force also packaging the Asts */
   lazy val packageSettings = Keys.`package` in Compile := {
     (Keys.`package` in Compile).value
     (packageAst in Compile).value
